3edc8951a5b4ffe43a48ed89f4497a606d40d803
VolkySe <sergej_lykov@mail.ru>
2025-03-13 11:31:35 +0300

    Initial commit for rebuliding project, because PR mistake. History here https://github.com/VolkySe/HWCollectionsv2/tree/HWStreamAndOptional

---
+++ src/main/java/ru/skypro/StreamAPIhomework/StreamApIhomeworkApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.StreamAPIhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApIhomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamApIhomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/StreamAPIhomework/StreamApIhomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.StreamAPIhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApIhomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
